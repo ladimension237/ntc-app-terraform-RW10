927005b64dc2902e49620aaebfc1f4e83588e268
ramses <ramsesmeumeu3@gmail.com>
2025-10-01 21:37:34 -0400

    infracost tk2

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ sudo systemctl enable httpd
 # Get the hostname first
 HOSTNAME=$(hostname)
 echo "<html><h1><p> ${HOSTNAME}</p></h1></html>" | sudo tee /var/www/html/index.html
+
+
+
+# At work we have couple engineer running terraform code locally after making changes to the tf file. the goal is to set up some type of automation to help run those commands. 
+# since we are using github action already , do some research on how we can execute those terraform command thru a ci/cd pipeline. 
+# also check if there are some terraform scaning tool that can be integrated in the pipeline
